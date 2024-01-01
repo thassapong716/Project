aa35c7d4f7f32c2400739e48acb7cec55d8b97ce
PC-Uway <155339478+PC-Uway@users.noreply.github.com>
2023-12-31 20:05:32 -0800

    Update MyNavBar.js
    Changed it to the new MyNavBar with FAQ

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -1,22 +1,64 @@
-//import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+//import '../style.css';
+
 const MyNavbar = () => {
-    return (
-        <nav className="navbar navbar-dark navbar-expand-sm fixed-top" style={{backgroundColor: '#bd5d38'}}>
-        <div className="container">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
-                <span className="navbar-toggler-icon"> </span>
-            </button>
-           <div className="collapse navbar-collapse" id="Navbar">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active"><a className="nav-link" to="/about"><span className="fa fa-home fa-lg"></span> Menu1</a></li>
-                    <li className="nav-item"><a className="nav-link" to="/acads"><span className="fa fa-list fa-lg"></span>Menu2</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#techskills"><span className="fa fa-list fa-lg"></span>Menu3</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#projects"><span className="fa fa-info fa-lg"></span> Menu4</a></li>
-                 </ul>       
+    return ( 
+        <div className="container-fluid">
+        
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary sticky-top">
+            <a className="navbar-brand" href="faq.html"><img src="imgs/BlueFlower-2.png" alt="LOGO"/></a>
+            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation"></button>
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+                <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li className="nav-item mx-4 my-4">
+                        <a className="nav-link" href="bulletin.html">Bulletin-Board</a>
+                    </li>
+                    <li className="nav-item mx-4 my-4">
+                        <a className="nav-link" href="loadForm.html">Upload-Posts</a>
+                    </li>
+                    <li className="nav-item mx-4 my-4">
+                        <a className="nav-link" href="faq.html">FAQ</a>
+                    </li>
+                   
+                </ul>         
+                <div className="d-flex me-2">
+                    <button type="button" className="btn btn-warning btn-lg" data-bs-toggle="modal" data-bs-target="#modalId"> Login </button>
+                </div>        
+            </div>
+        </nav>
+
+{/*-- Modal Body --*/}
+{/*-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard */}
+<div className="modal fade" id="modalId" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="modalTitleId">Login </h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                Body
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save</button>
             </div>
         </div>
-        </nav>
-      );
+    </div>
+</div>
+
+{/*-- Optional: Place to the bottom of scripts */}
+<script>
+    const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
+</script>
+
+<br/>
+<footer> Teamup &copy;  2023</footer>      
+
+    </div>
+
+     );
 }
  
 export default MyNavbar;
